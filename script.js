2a923774a492ea8b60666b000b7a920470e6f864
kannan <133184797+rs-kannan@users.noreply.github.com>
2023-08-11 15:34:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let timer =10;
+let time = document.getElementById('display')
+time.style.margin="auto";
+time.style.textAlign="center";
+time.style.maxWidth="200px";
+setTimeout(()=>{
+  time.innerHTML=timer--;
+  setTimeout(()=>{
+    time.className="badge rounded-pill bg-light text-dark";
+    time.innerHTML=timer--;
+    setTimeout(()=>{
+      time.className="badge rounded-pill bg-danger";
+      time.innerHTML=timer--;
+      setTimeout(()=>{
+        time.className="badge rounded-pill bg-light text-dark";
+        time.innerHTML=timer--;
+        setTimeout(()=>{
+          time.className="badge rounded-pill bg-danger"
+          time.innerHTML=timer--;
+          setTimeout(()=>{
+            time.className="badge rounded-pill bg-light text-dark";
+            time.innerHTML=timer--;
+            setTimeout(()=>{
+              time.className="badge rounded-pill bg-danger"
+              time.innerHTML=timer--;
+              setTimeout(()=>{
+                time.className="badge rounded-pill bg-light text-dark";
+                time.innerHTML=timer--;
+                setTimeout(()=>{
+                  time.className="badge rounded-pill bg-danger"
+                  time.innerHTML=timer--;
+                  setTimeout(()=>{
+                    time.className="badge rounded-pill bg-light text-dark";
+                    time.innerHTML=timer--;
+                      setTimeout(()=>{
+                        time.style.fontSize="20px";
+                        time.style.maxWidth="310px";
+                        time.className="badge rounded-pill bg-danger"
+                        time.innerHTML="Happy Independence day !"
+                      },1000)
+                    },1000)
+                  },1000)
+                },1000)
+              },1000)
+            },1000)
+          },1000)
+        },1000)
+      },1000)
+    },1000)
+  },1000)
